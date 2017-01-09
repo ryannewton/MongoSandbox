ab074f03e74626dbb336f6ffcab3f1d6b9d06756
Ryan Newton <newton1988@gmail.com>
2017-01-09 17:53:55 -0500

    Ensures test are run after connection is made to DB

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 // Replace mongoose's promise library with the ES6 Promise
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-	.once('open', () => {})
-	.on('error', (error) => {
-		console.warn('Warning', error);
-	});
+before((done) => {
+	mongoose.connect('mongodb://localhost/users_test');
+	mongoose.connection
+		// Waits until connection is made to run tests
+		.once('open', () => { done(); })
+		.on('error', (error) => {
+			console.warn('Warning', error);
+		});
+});
 
 beforeEach((done) => {
 	mongoose.connection.collections.users.drop(() => {
