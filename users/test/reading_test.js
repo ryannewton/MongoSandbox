b0173289993a95d9a49d647a78d1230b4f15218c
Ryan Newton <newton1988@gmail.com>
2017-01-09 18:38:09 -0500

    Adds test to find user by id

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -12,9 +12,17 @@ describe('Reading users out of the database', () => {
 	it('finds all users with a name of joe', (done) => {
 		User.find({ name: 'Joe' })
 			.then((users) => {
-				// Gotcha note: _id is saves as an ObjectId object. To compare, use the .toString() function
+				// Gotcha note: _id is saved as an ObjectId object. To compare, use the .toString() function
 				assert(users[0]._id.toString() === joe._id.toString());
 				done();
 			})
+	});
+
+	it('find a user with a particular id', (done) => {
+		User.findOne({ _id: joe._id })
+			.then((user) => {
+				assert(user.name === 'Joe');
+				done();
+			});
 	});
 });
