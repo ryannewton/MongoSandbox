8e65f891707f78ecd34d76d4fbd15fa6c0311eb2
Ryan Newton <newton1988@gmail.com>
2017-01-09 18:25:47 -0500

    Adds first test for reading from DB

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -9,7 +9,12 @@ describe('Reading users out of the database', () => {
 			.then(() => done());
 	});
 
-	it('finds all users with a name of joe', () => {
-		// Add test to look for users with name joe
+	it('finds all users with a name of joe', (done) => {
+		User.find({ name: 'Joe' })
+			.then((users) => {
+				// Gotcha note: _id is saves as an ObjectId object. To compare, use the .toString() function
+				assert(users[0]._id.toString() === joe._id.toString());
+				done();
+			})
 	});
 });
