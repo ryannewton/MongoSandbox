f97ae1b7192b666461a3f76696a27076ffd732c8
Ryan Newton <newton1988@gmail.com>
2017-01-10 17:50:27 -0500

    Modifies test for incrementing many instances (postCount -> likes)

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 	let joe;
 
 	beforeEach((done) => {
-		joe = new User({ name: 'Joe' });
+		joe = new User({ name: 'Joe', likes: 0 } );
 		joe.save()
 			.then(() => done());
 	});
@@ -55,11 +55,11 @@ describe('Updating records', () => {
 		);
 	});
 
-	xit('A user can increment postcount by 1', (done) => {
-		User.update({ name: 'Joe' }, { "$inc": { postCount: 1 }})
+	it('A user can increment postcount by 1', (done) => {
+		User.update({ name: 'Joe' }, { "$inc": { likes: 1 }})
 			.then(() => User.findOne({ name: 'Joe' }))
 			.then((user) => {
-				assert(user.postCount === 1);
+				assert(user.likes === 1);
 				done();
 			});
 	});
