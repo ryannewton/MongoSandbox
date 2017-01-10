edec789e7140a6a93d2d1ee70430e947f7f7a42e
Ryan Newton <newton1988@gmail.com>
2017-01-10 17:10:15 -0500

    Makes postCount a virtual attribute

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 	let joe;
 
 	beforeEach((done) => {
-		joe = new User({ name: 'Joe', postCount: 0 });
+		joe = new User({ name: 'Joe' });
 		joe.save()
 			.then(() => done());
 	});
@@ -55,7 +55,7 @@ describe('Updating records', () => {
 		);
 	});
 
-	it('A user can increment postcount by 1', (done) => {
+	xit('A user can increment postcount by 1', (done) => {
 		User.update({ name: 'Joe' }, { "$inc": { postCount: 1 }})
 			.then(() => User.findOne({ name: 'Joe' }))
 			.then((user) => {
