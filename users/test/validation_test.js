7f414d4c331727bfcd1336bbc1ca6adc66e3a7a1
Ryan Newton <newton1988@gmail.com>
2017-01-10 13:09:31 -0500

    Adds test to ensure User.name is >2 characters

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -2,12 +2,19 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
-	it('requires a user name', () => {
+	it('requires a user\'s name', () => {
 		const user = new User({ name: undefined });
 		// Note: validateSync() is a synchronous version of validate()
 		// Since this doesn't access a DB, no need for using asynchronous code
 		const validationResult = user.validateSync();
 		const { message } = validationResult.errors.name;
 		assert(message === 'Name is required.');
+	});
+
+	it('requires a user\'s name longer than 2 characters', () => {
+		const user = new User({ name: 'Al' });
+		const validationResult = user.validateSync();
+		const { message } = validationResult.errors.name;
+		assert(message === 'Name must be longer than 2 characters.');
 	});
 });
