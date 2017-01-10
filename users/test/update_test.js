373386cddaa0b145dfaa37394b2e30c5756a9706
Ryan Newton <newton1988@gmail.com>
2017-01-09 19:36:11 -0500

    Adds a test for updating the DB using set() and save()

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -10,4 +10,15 @@ describe('Updating records', () => {
 			.then(() => done());
 	});
 
+	it('instance type using set() & save()', (done) => {
+		// Set a new value for name, then save it
+		joe.set('name', 'Alex');
+		joe.save()
+			.then(() => User.find({}))
+			.then((users) => {
+				assert(users.length === 1);
+				assert(users[0].name === 'Alex');
+				done();
+			});
+	});
 });
