0985ffac2b0f9b7d6f71c50050ed09d58f0be3ab
Ryan Newton <newton1988@gmail.com>
2017-01-09 21:16:14 -0500

    Moves code that will repeated into a function

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -10,15 +10,19 @@ describe('Updating records', () => {
 			.then(() => done());
 	});
 
-	it('instance type using set() & save()', (done) => {
-		// Set a new value for name, then save it
-		joe.set('name', 'Alex');
-		joe.save()
+	function assertName(operation, done) {
+		operation
 			.then(() => User.find({}))
 			.then((users) => {
 				assert(users.length === 1);
 				assert(users[0].name === 'Alex');
 				done();
 			});
+	}
+
+	it('instance type using set() & save()', (done) => {
+		// Set a new value for name, then save it
+		joe.set('name', 'Alex');
+		assertName(joe.save(), done);
 	});
 });
