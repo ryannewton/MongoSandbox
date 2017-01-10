2b08b5c4bb7a66aaed9f68af5c478dbee870353f
Ryan Newton <newton1988@gmail.com>
2017-01-10 13:06:14 -0500

    Adds test to ensure User.name is required

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -2,5 +2,12 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
-
+	it('requires a user name', () => {
+		const user = new User({ name: undefined });
+		// Note: validateSync() is a synchronous version of validate()
+		// Since this doesn't access a DB, no need for using asynchronous code
+		const validationResult = user.validateSync();
+		const { message } = validationResult.errors.name;
+		assert(message === 'Name is required.');
+	});
 });
