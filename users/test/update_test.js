389a3f3739bac679bf38d70286211d75962693c8
Ryan Newton <newton1988@gmail.com>
2017-01-09 19:24:54 -0500

    Adds boilerplate for testing updates to DB

---
+++ users/test/update_test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+	let joe;
+
+	beforeEach((done) => {
+		joe = new User({ name: 'Joe' });
+		joe.save()
+			.then(() => done());
+	});
+
+});
