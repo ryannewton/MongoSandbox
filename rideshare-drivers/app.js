05c964c9f7d6c1c036a1f696c4dcbd74a513cda3
Ryan Newton <newton1988@gmail.com>
2017-01-13 16:45:08 -0500

    Connects app to a MongoDB

--- rideshare-drivers/app.js
+++ rideshare-drivers/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/rideshare-drivers');
 
 app.use(bodyParser.json());
 routes(app);
